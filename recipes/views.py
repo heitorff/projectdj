3ea768db3415116f4e316234192831dd51cec97f
Heitor Figueiredo Fernandes <heitorffernandes@gmail.com>
2025-03-25 17:38:45 -0300

    Adding and modifying home page

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request, *args, **kwargs):
-    return render(request, 'recipes/home.html', {})
-
-def contato(request, *args, **kwargs):
-    return HttpResponse('Contato')
-
-def sobre(request, *args, **kwargs):
-    return HttpResponse('Sobre')
+    return render(request, 'recipes/pages/home.html', {})
